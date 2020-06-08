c550a3d122f1a6012856035a44ecaf31473357ce
Blacks <mj_ol@outlook.com>
2020-06-09 00:12:22 +0800

    INIT

---
+++ project/demo/src/test/java/com/jecosine/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecosine.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
